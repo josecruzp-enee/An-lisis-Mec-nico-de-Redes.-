4257f4375c4d6203adc6ec3f1976ca0db1d8eb7b
josecruzp-enee <j90nikol@gmail.com>
2026-01-17 20:01:21 -0600

    Update engine.py

--- analisis/engine.py
+++ analisis/engine.py
@@ -35,7 +35,8 @@ def ejecutar_fase_geometria(df: pd.DataFrame) -> Dict[str, pd.DataFrame]:
             deflex_real_list.append(deflex_real)
 
         # Guardar la deflexión real (reemplaza la anterior)
-        df_def["Deflexión (°)"] = df_def["Deflexión (°)"].round(2)
+        df_def["Deflexión (°)"] = deflex_real_list
+        df_def["Deflexión (°)"] = df_def["Deflexión (°)"].round(1)
 
 
         # Clasificar con deflexión real
